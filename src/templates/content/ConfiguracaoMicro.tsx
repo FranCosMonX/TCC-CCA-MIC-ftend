5973fa15caa42b5544955e30aaf4a844c373683b
Francisco C <debian.programmer.francisco@gmail.com>
2026-06-16 00:21:05 -0300

    corrigindo bug de altura indefinida na lista de mics

--- src/templates/content/ConfiguracaoMicro.tsx
+++ src/templates/content/ConfiguracaoMicro.tsx
@@ -154,6 +154,14 @@ const ConfiguracaoMicro: React.FC<ConfiguracaoMicroParams> = ({ closeModal, open
                 onChange={handleChangeMicrocontrolador}
                 displayEmpty
                 inputProps={{ 'aria-label': 'Without label' }}
+                MenuProps={{
+                  PaperProps: {
+                    style: {
+                      maxHeight: 48 * 5,
+                      minHeight: 48 * 3,
+                    },
+                  },
+                }}
               >
                 <MenuItem value="">
                   <em>Nenhum</em>
